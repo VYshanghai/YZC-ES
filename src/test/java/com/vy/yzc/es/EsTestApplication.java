f434dc4d6dae6a0e0e6397327ed57a022e2c5bdc
edward <lsd0421at@aliyun.com>
2021-03-30 16:07:28 +0800

    添加测试方法

--- src/test/java/com/vy/yzc/es/EsTestApplication.java
+++ src/test/java/com/vy/yzc/es/EsTestApplication.java
@@ -1,8 +1,21 @@
 package com.vy.yzc.es;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vy.yzc.es.dal.repo.OffersRepository;
 import com.vy.yzc.es.dal.repo.model.EsOffersPO;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import org.assertj.core.util.Lists;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.GetMappingsRequest;
+import org.elasticsearch.client.indices.GetMappingsResponse;
+import org.elasticsearch.cluster.metadata.MappingMetaData;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.junit.Test;
@@ -10,6 +23,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
@@ -24,6 +38,12 @@ public class EsTestApplication {
 	@Autowired
 	private OffersRepository offersRepository;
 
+	/*@Autowired
+	private ElasticsearchTemplate elasticsearchTemplate;*/
+
+	@Autowired
+	private RestHighLevelClient restHighLevelClient;
+
 	@Test
 	public void test(){
 
@@ -33,6 +53,38 @@ public class EsTestApplication {
 
 		Lists.newArrayList(search).forEach(System.out::println);
 //		System.out.println(offersRepository.findById(1375070232629309470L));
+	}
+
+	@Test
+	public void test2() throws IOException {
+
+//		Map<String, Object> mapping = elasticsearchTemplate.getMapping("visva-yzc-beta", "offers");
+
+		GetMappingsRequest getMappings = new GetMappingsRequest().indices("visva-yzc-beta");
+		//调用获取
+		GetMappingsResponse getMappingResponse = restHighLevelClient.indices()
+				.getMapping(getMappings, RequestOptions.DEFAULT);
+		//处理数据
+		Map<String, MappingMetaData> allMappings = getMappingResponse.mappings();
+		List<Map<String, Object>> mapList = new ArrayList<>();
+		for (Map.Entry<String, MappingMetaData> indexValue : allMappings.entrySet()) {
+			Map<String, Object> mapping = indexValue.getValue().sourceAsMap();
+			Iterator<Entry<String, Object>> entries = mapping.entrySet().iterator();
+			entries.forEachRemaining(stringObjectEntry -> {
+				if (stringObjectEntry.getKey().equals("properties")) {
+					Map<String, Object> value = (Map<String, Object>) stringObjectEntry.getValue();
+					for (Map.Entry<String, Object> ObjectEntry : value.entrySet()) {
+						Map<String, Object> map = new HashMap<>();
+						String key = ObjectEntry.getKey();
+						Map<String, Object> value1 = (Map<String, Object>) ObjectEntry.getValue();
+						map.put(key, value1.get("type"));
+						mapList.add(map);
+					}
+				}
+			});
+		}
+		ObjectMapper objectMapper = new ObjectMapper();
+		System.out.println(objectMapper.writeValueAsString(mapList));
 	}
 
 }
